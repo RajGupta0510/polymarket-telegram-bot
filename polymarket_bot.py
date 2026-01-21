6659a4c7c2b3b51313bac85af142f375de516880
Raj Gupta <rajaryangupta5@gmail.com>
2026-01-21 20:11:46 +0530

    Update polymarket_bot.py

--- polymarket_bot.py
+++ polymarket_bot.py
@@ -18,7 +18,7 @@ POLYMARKET_TRADES_API = "https://data-api.polymarket.com/trades?limit=500"
 POLYMARKET_MARKETS_API = "https://data-api.polymarket.com/markets?limit=100"
 
 CHECK_INTERVAL = 5
-MIN_TRADE_USD = 20   # 🔧 CHANGE WHEN NEEDED
+MIN_TRADE_USD = 250   # 🔧 CHANGE WHEN NEEDED
 
 # =====================
 # STATE
@@ -220,3 +220,4 @@ while True:
         print("Error:", e)
 
     time.sleep(CHECK_INTERVAL)
+
